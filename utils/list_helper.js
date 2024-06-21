96b907102367eb6b8224d6bb56523aad8f168080
Nataleesha <frojlan@gmail.com>
2024-06-21 18:01:45 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const mostLikes = blogs.reduce((prev, curr) => {
+    return prev && prev.likes > curr.likes ? prev : curr;
+  });
+
+  const { title, author, likes } = mostLikes;
+
+  console.log("THE MOST LIKED BLOG: ", { title, author, likes });
+
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
