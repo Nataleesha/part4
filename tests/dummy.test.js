4b8440b81d60e0b6f6d6eb714b794e938969bc16
Nataleesha <frojlan@gmail.com>
2024-06-21 17:17:29 +0300

    4.4

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -8,3 +8,45 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      id: "5",
+      likes: 5,
+    },
+  ];
+
+  const listWithMoreBlogs = [
+    {
+      id: "1",
+      likes: 1,
+    },
+    {
+      id: "2",
+      likes: 2,
+    },
+    {
+      id: "3",
+      likes: 3,
+    },
+    {
+      id: "4",
+      likes: 4,
+    },
+  ];
+
+  test("of an empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithMoreBlogs);
+    assert.strictEqual(result, 10);
+  });
+});
